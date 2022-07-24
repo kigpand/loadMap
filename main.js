02743dbc55981f845d64d1e1e841cb34faf2d27a
kigpand <kigpand@naver.com>
2022-07-24 20:18:58 +0900

    control 적용

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 function init() {
     const scene = new THREE.Scene();
@@ -8,26 +9,72 @@ function init() {
         0.1,
         1000
     );
+    camera.position.y = 1.5;
+	camera.position.z = 4;
+    scene.add(camera);
+
+    const ambientLight = new THREE.AmbientLight('white', 0.5);
+	scene.add(ambientLight);
+
+	const directionalLight = new THREE.DirectionalLight('white', 1);
+	directionalLight.position.x = 1;
+	directionalLight.position.z = 2;
+	scene.add(directionalLight);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
     document.body.appendChild(renderer.domElement);
 
     const planeGeometry = new THREE.PlaneGeometry( 10, 10 );
     const planeMaterial = new THREE.MeshBasicMaterial( {color: '#0000ff', side: THREE.DoubleSide} );
     const plane = new THREE.Mesh( planeGeometry, planeMaterial );
-    plane.rotateX(90);
+    plane.rotation.x = THREE.MathUtils.degToRad(90);
     scene.add( plane );
 
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
 
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    let mesh;
+    let material;
+    for (let i = 0; i< 20; i++) {
+        material = new THREE.MeshStandardMaterial({
+            color: `rgb(
+                ${Math.floor(Math.random() * 255)},
+                ${Math.floor(Math.random() * 255)},
+                ${Math.floor(Math.random() * 255)}
+            )`
+        });
+        mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = (Math.random() - 0.5) * 5;
+        mesh.position.y = 0;
+        mesh.position.z = (Math.random() - 0.5) * 5;
+        scene.add(mesh);
+    }
 
-    camera.position.z = 10;
+    const clock = new THREE.Clock();
 
-    renderer.render(scene, camera);
+    function draw() {
+        const delta = clock.getDelta();
+
+        controls.update();
+
+        renderer.render(scene, camera);
+        renderer.setAnimationLoop(draw);
+    }
+
+    function setSize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.render(scene, camera);
+	}
+
+	// 이벤트
+	window.addEventListener('resize', setSize);
+
+    draw();
 }
 
 init();
